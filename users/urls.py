fa7e794f5fb9e6f81509f676150ee0f3b68db610
KWONMINWOOK <godsla1125@gmail.com>
2022-04-28 10:09:34 +0900

    login logout update

--- users/urls.py
+++ users/urls.py
@@ -10,8 +10,8 @@ from django.conf.urls.static import static
 app_name = "user"
 
 urlpatterns = [
-    path("login", views.login_view, name="login"),
+    path("", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("signup", views.signup_view, name="signup"),
-    path("", views.index, name='index'),
+    #path("", views.index, name='index'),
 ]
